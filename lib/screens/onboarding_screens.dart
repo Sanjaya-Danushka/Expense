23cd78c9b67c621116ceca43170e51b854ec0166
sanjaya <62214882+Sanjaya-Danushka@users.noreply.github.com>
2025-06-09 22:30:15 +0530

    userscreen

--- lib/screens/onboarding_screens.dart
+++ lib/screens/onboarding_screens.dart
@@ -3,6 +3,7 @@ import 'package:projectzero/constants/colors.dart';
 import 'package:projectzero/data/onboarding_data.dart';
 import 'package:projectzero/screens/onbarding/front_page.dart';
 import 'package:projectzero/screens/onbarding/shard_onnboarding_screens.dart';
+import 'package:projectzero/screens/user_datascreen.dart';
 import 'package:projectzero/widgets/custome_button.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -68,19 +69,36 @@ class _OnboardingScreensState extends State<OnboardingScreens> {
                   right: 0,
                   child: Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 30),
-                    child: GestureDetector(
-                      onTap: () {
-                        _pageController.nextPage(
-                          duration: const Duration(milliseconds: 300),
-                          curve: Curves.easeInOut,
-                        );
-                        if (_pageController.page == 3) {
-                          showDetailsPage = true;
-                          print("done");
-                        }
-                      },
-                      child: CustomButton(text: 'Next', color: kMainColor),
-                    ),
+                    child: !showDetailsPage
+                        ? GestureDetector(
+                            onTap: () {
+                              _pageController.nextPage(
+                                duration: const Duration(milliseconds: 300),
+                                curve: Curves.easeInOut,
+                              );
+                              if (_pageController.page == 3) {
+                                showDetailsPage = true;
+                              }
+                            },
+                            child: CustomButton(
+                              text: 'Next',
+                              color: kMainColor,
+                            ),
+                          )
+                        : GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const UserDatascreen(),
+                                ),
+                              );
+                            },
+                            child: CustomButton(
+                              text: 'Done',
+                              color: kMainColor,
+                            ),
+                          ),
                   ),
                 ),
               ],
