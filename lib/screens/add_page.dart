2bcb9ca7bc43ad4f2043d3c69730eec4c7468d3f
sanjaya <62214882+Sanjaya-Danushka@users.noreply.github.com>
2025-06-13 22:15:59 +0530

    expense income form

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:projectzero/constants/colors.dart';
 import 'package:projectzero/constants/constants.dart';
+import 'package:projectzero/models/expense_model.dart';
+import 'package:projectzero/models/income_model.dart';
 
 class AddNewScreen extends StatefulWidget {
   const AddNewScreen({super.key});
@@ -12,20 +14,23 @@ class AddNewScreen extends StatefulWidget {
 class _AddNewScreenState extends State<AddNewScreen> {
   //state for the selected tab
   int selectedTab = 0;
+  ExpenseCategory expenseCategory = ExpenseCategory.food;
+  IncomeCategory incomeCategory = IncomeCategory.salary;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: selectedTab == 0 ? kRed : kGreen,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
-              child: Stack(
-                children: [
-                  Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(kDefaultPadding),
+                  child: Container(
+                    //expense and income menu
                     height: MediaQuery.of(context).size.height * 0.06,
                     width: double.infinity,
                     decoration: BoxDecoration(
@@ -92,8 +97,173 @@ class _AddNewScreenState extends State<AddNewScreen> {
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                //amount feild
+                Padding(
+                  padding: EdgeInsets.all(kDefaultPadding),
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.1,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        //title
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: kDefaultPadding,
+                          ),
+                          child: Text(
+                            'How much?',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w600,
+                              color: kLightGray.withAlpha(80),
+                            ),
+                          ),
+                        ),
+                        TextField(
+                          style: TextStyle(
+                            color: kWhite,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: '0',
+                            hintStyle: TextStyle(
+                              color: kWhite,
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //user data form
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.52,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.3,
+                  ),
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                  ),
+
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          //category selector
+                          DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefaultPadding,
+                                horizontal: 20,
+                              ),
+                              hintText: 'Select Category',
+                              hintStyle: TextStyle(
+                                color: kGray,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            items: selectedTab == 0
+                                ? ExpenseCategory.values.map((category) {
+                                    return DropdownMenuItem(
+                                      value: category,
+                                      child: Text(category.name),
+                                    );
+                                  }).toList()
+                                : IncomeCategory.values.map((category) {
+                                    return DropdownMenuItem(
+                                      value:category,
+                                      child: Text(category.name),
+                                    );
+                                  }).toList(),
+                            value: selectedTab == 0 ? expenseCategory : incomeCategory,
+                            onChanged: (value) {
+                              setState(() {
+                                selectedTab == 0
+                                    ? expenseCategory = value as ExpenseCategory
+                                    : incomeCategory = value as IncomeCategory;
+                              });
+                            },
+                          ),
+                          SizedBox(height: kDefaultPadding),
+                          //title
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefaultPadding,
+                                horizontal: 20,
+                              ),
+                              hintText: 'Title',
+                              hintStyle: TextStyle(
+                                color: kGray,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: kDefaultPadding),
+                          //description
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefaultPadding,
+                                horizontal: 20,
+                              ),
+                              hintText: 'Description',
+                              hintStyle: TextStyle(
+                                color: kGray,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: kDefaultPadding),
+                          //description
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefaultPadding,
+                                horizontal: 20,
+                              ),
+                              hintText: 'Amount',
+                              hintStyle: TextStyle(
+                                color: kGray,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
